fa375aad8cb6150e98059d30033db3ffbed3a7e5
Adam Lawson <alaws057@uottawa.ca>
2020-08-19 10:50:01 -0400

    Add commands

--- Migrations/20200819142607_commands.Designer.cs
+++ Migrations/20200819142607_commands.Designer.cs
@@ -3,14 +3,16 @@ using HelpfulCommands.Data.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HelpfulCommands.Migrations
 {
     [DbContext(typeof(HelpfulCommandsApiContext))]
-    partial class HelpfulCommandsApiContextModelSnapshot : ModelSnapshot
+    [Migration("20200819142607_commands")]
+    partial class commands
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
